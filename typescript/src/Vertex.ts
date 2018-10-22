61db37a7674598087ea10fb30099fd996e567a00
leonarr1 <leonarr1@tcd.ie>
2018-10-22 17:00:58 +0100

    added the structure for a graph

--- typescript/src/Vertex.ts
+++ typescript/src/Vertex.ts
@@ -7,8 +7,8 @@ export class Vertex {
 
 	private _id: number;
 
-	private _next: Vertex = null;
-	private _previous: Vertex = null;
+	public _next: Vertex = null;
+	public _previous: Vertex = null;
 
 	private _uplinks: Edge[] = [];
 	private _downlinks: Edge[] = [];
@@ -46,6 +46,86 @@ export class Vertex {
 	/** Gets the first vertex in the chain. */
 	get first(): Vertex { 
 		return this._previous ? this._previous.first : this; 
+	}
+
+	/**
+	 * Inserts the given vertex directly after this one.
+	 * @param {Vertex} vertex The vertex to insert.
+	 */
+	insertAfter(vertex: Vertex): Vertex {
+		const next = this._next;
+		Vertex.join(vertex._previous, vertex._next);
+		Vertex.join(this, vertex);
+		Vertex.join(vertex, next);
+		return vertex;
+	}
+
+	/**
+	 * Connects this vertex to the given vertex, returning a new instance of edge which defines the
+	 * relationship between the two vertices.
+	 */
+	connectTo(vertex: Vertex, id: number = 0): Edge {
+		for (let e of this._downlinks) {
+			if (e.bottom === vertex) {
+				throw new Error("The vertices are already connected.");
+			}
+		}
+		const e: Edge = {
+			id: id,
+			top: this,
+			bottom: vertex,
+			content: null
+		};
+		this._downlinks.push(e);
+		vertex._uplinks.push(e);
+		return e;
+	}
+
+	/**
+	 * Returns an array of vertices to which this vertex direectly connects.
+	 */
+	directlyBelow(): Vertex[] {
+		return this._downlinks.map(e => e.bottom);
+	}
+
+	/**
+	 * Returns an array of vertices to which this vertex connects directly or indirectly.
+	 */
+	below(): Vertex[] {
+		return this.directlyBelow().reduce((prev, cur) => {
+			return [ ...prev, cur, ...cur.directlyBelow() ];
+		}, []);
+	}
+
+	/**
+	 * Returns true if the given vertex is a direct or indirect downlink of this vertex.
+	 */
+	isBelow(vertex: Vertex): boolean {
+		if (vertex === this) {
+			return false;
+		}
+		const cb = (e, i, arr) => e === vertex;
+		return this.below().some(cb, this);
+	}
+
+	/**
+	 * Returns an array of possible edges that could be created without creating a cyclical
+	 * connection.
+	 */
+	availableConnections(): ProtoEdge[] {
+		const protos: ProtoEdge[] = [];
+		const isConnected = (v: Vertex) => this._downlinks.some(e => e.bottom === v);
+		let pointer = this.first;
+		while (pointer) {
+			if (pointer !== this && !pointer.isBelow(this) && !isConnected(pointer)) {
+				protos.push({
+					top: this,
+					bottom: pointer
+				});
+			}
+			pointer = pointer._next;
+		}
+		return protos;
 	}
 
 	/**
