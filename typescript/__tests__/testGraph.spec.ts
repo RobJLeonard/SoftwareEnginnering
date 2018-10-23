f1c4966005c9e6c917ad02fc0081ead5d649cc59
leonarr1 <leonarr1@tcd.ie>
2018-10-23 20:53:21 +0100

    added test file for dag graph

---
+++ typescript/__tests__/testGraph.spec.ts
@@ -0,0 +1,20 @@
+import { Graph } from '../src/Graph';
+import { expect } from 'chai';
+import 'jest';
+
+
+describe('Create DAG Graph', () => {
+
+    it('Should create a graph node', () => {
+        let graph = new Graph();
+
+        let v1 = graph.addVertex();
+
+        let v2 = graph.addVertex();
+
+        
+        graph.addEdge(v1, v2);
+
+        expect(graph.vertices).to.be.not.null;
+    });
+});
